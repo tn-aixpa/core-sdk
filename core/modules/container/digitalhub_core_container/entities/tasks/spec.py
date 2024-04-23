98c6a57c11d5fbaae19fff7e628c07e2ee4570a9
EvangMM <matteomartini92@yahoo.it>
2024-04-23 11:35:54 +0200

    fix on task k8s specs

--- core/modules/container/digitalhub_core_container/entities/tasks/spec.py
+++ core/modules/container/digitalhub_core_container/entities/tasks/spec.py
@@ -22,18 +22,18 @@ class TaskSpecJob(TaskSpec):
         super().__init__(function)
         if k8s is None:
             k8s = {}
-        k8s = K8s(**k8s)
-        self.node_selector = k8s.node_selector
-        self.volumes = k8s.volumes
-        self.resources = k8s.resources
-        self.affinity = k8s.affinity
-        self.tolerations = k8s.tolerations
-        self.labels = k8s.labels
-        self.env = k8s.env
-        self.secrets = k8s.secrets
-        self.backoff_limit = k8s.backoff_limit
-        self.schedule = k8s.schedule
-        self.replicas = k8s.replicas
+        k8s = K8s(**k8s).dict(by_alias=True)
+        self.node_selector = k8s.get("node_selector")
+        self.volumes = k8s.get("volumes")
+        self.resources = k8s.get("resources")
+        self.affinity = k8s.get("affinity")
+        self.tolerations = k8s.get("tolerations")
+        self.labels = k8s.get("labels")
+        self.env = k8s.get("env")
+        self.secrets = k8s.get("secrets")
+        self.backoff_limit = k8s.get("backoff_limit")
+        self.schedule = k8s.get("schedule")
+        self.replicas = k8s.get("replicas")
 
 
 class TaskParamsJob(TaskParams):
@@ -59,18 +59,18 @@ class TaskSpecDeploy(TaskSpec):
         super().__init__(function)
         if k8s is None:
             k8s = {}
-        k8s = K8s(**k8s)
-        self.node_selector = k8s.node_selector
-        self.volumes = k8s.volumes
-        self.resources = k8s.resources
-        self.affinity = k8s.affinity
-        self.tolerations = k8s.tolerations
-        self.labels = k8s.labels
-        self.env = k8s.env
-        self.secrets = k8s.secrets
-        self.backoff_limit = k8s.backoff_limit
-        self.schedule = k8s.schedule
-        self.replicas = k8s.replicas
+        k8s = K8s(**k8s).dict(by_alias=True)
+        self.node_selector = k8s.get("node_selector")
+        self.volumes = k8s.get("volumes")
+        self.resources = k8s.get("resources")
+        self.affinity = k8s.get("affinity")
+        self.tolerations = k8s.get("tolerations")
+        self.labels = k8s.get("labels")
+        self.env = k8s.get("env")
+        self.secrets = k8s.get("secrets")
+        self.backoff_limit = k8s.get("backoff_limit")
+        self.schedule = k8s.get("schedule")
+        self.replicas = k8s.get("replicas")
 
 
 class TaskParamsDeploy(TaskParams):
@@ -98,18 +98,18 @@ class TaskSpecServe(TaskSpec):
         super().__init__(function)
         if k8s is None:
             k8s = {}
-        k8s = K8s(**k8s)
-        self.node_selector = k8s.node_selector
-        self.volumes = k8s.volumes
-        self.resources = k8s.resources
-        self.affinity = k8s.affinity
-        self.tolerations = k8s.tolerations
-        self.labels = k8s.labels
-        self.env = k8s.env
-        self.secrets = k8s.secrets
-        self.backoff_limit = k8s.backoff_limit
-        self.schedule = k8s.schedule
-        self.replicas = k8s.replicas
+        k8s = K8s(**k8s).dict(by_alias=True)
+        self.node_selector = k8s.get("node_selector")
+        self.volumes = k8s.get("volumes")
+        self.resources = k8s.get("resources")
+        self.affinity = k8s.get("affinity")
+        self.tolerations = k8s.get("tolerations")
+        self.labels = k8s.get("labels")
+        self.env = k8s.get("env")
+        self.secrets = k8s.get("secrets")
+        self.backoff_limit = k8s.get("backoff_limit")
+        self.schedule = k8s.get("schedule")
+        self.replicas = k8s.get("replicas")
         self.service_ports = service_ports
         self.service_type = service_type
 

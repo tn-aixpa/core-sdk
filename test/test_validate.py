0209cac469b3d519d272a2860292d0474dcbc42a
alb-car <alb.carotti@gmail.com>
2024-11-20 10:49:27 +0100

    test: updated validation script to pytest

---
+++ test/test_validate.py
@@ -0,0 +1,53 @@
+import os
+import pytest
+from glob import glob
+from pathlib import Path
+import json
+from jsonschema import validate
+from digitalhub.factory.factory import factory
+
+entities_path = "/path/to/items/to/validate"
+schema_path = "/path/to/schemas"
+
+# Build dict: kind -> path to schema file
+schemas = {}
+for path_to_schema in glob(f"{schema_path}/**/*.json", recursive=True):
+    kind = Path(path_to_schema).stem
+    schemas[kind] = path_to_schema
+
+# Build dict: name of file to validate -> full path to file}
+entity_paths = {}
+for path_to_file in glob(f"{entities_path}/**/*.json", recursive=True):
+    file_name = os.path.basename(path_to_file)
+
+    # If a file in a nested directory causes a name collision, use its full path as name 
+    if file_name in entity_paths:
+        file_name = path_to_file
+
+    entity_paths[file_name] = path_to_file
+
+# Build object from JSON file using factory
+def build_obj(entity_file_path):
+    with open(entity_file_path) as f:
+        entity = json.load(f)
+        
+    kind = entity["kind"]
+    spec = entity["spec"]
+
+    built = factory.build_spec(kind, **spec)
+    return built.to_dict(), kind
+
+# Validate built object against its kind's schema
+def is_valid(built, kind):
+    with open(schemas[kind]) as schema_file:
+        schema = json.load(schema_file)
+
+    validate(instance=built, schema=schema)
+    return True
+
+# Tests
+class TestValidate:
+    @pytest.mark.parametrize('file_name', list(entity_paths.keys()))
+    def test_validate(self, file_name):
+        built, kind = build_obj(f"{entity_paths[file_name]}")
+        assert is_valid(built, kind)

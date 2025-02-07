1123b7904f4f638b8fff008b755c8668895932b1
EvangMM <matteomartini92@yahoo.it>
2025-02-07 10:55:23 +0100

    ci: fixes on changelog

--- .github/scripts/changelog.sh
+++ .github/scripts/changelog.sh
@@ -1,1 +1,7 @@
-git-cliff --unreleased --tag ${1}  -c .github/scripts/cliff.toml -v -o
+#!/bin/bash
+
+if [ "$2" = "true" ]; then
+  touch CHANGELOG.md
+else
+  git-cliff --unreleased --tag ${1}  -c .github/scripts/cliff.toml -v -o
+fi
